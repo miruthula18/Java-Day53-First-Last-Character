0f06da13f3077583d05c05de2bd490d7d69ea7d0
miruthula <miruthula183@gmail.com>
2026-08-27 09:28:46 -0700

    Day 53 - Check First and Last Character

---
+++ FirstLastSame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FirstLastSame {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        if (str.charAt(0) == str.charAt(str.length() - 1)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Not Same");
+        }
+    }
+}
